a36d8f688edce700db81e92914b9d2610eb880c3
Oleg <n7o.nto@yandex.ru>
2026-06-10 18:49:32 +0300

    Added graph structure

---
+++ vishnevskiy.oleg/S3/Graph.hpp
@@ -0,0 +1,51 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+#include "HashTableImpl.hpp"
+#include <string>
+#include <cstddef>
+#include <boost/hash2/siphash.hpp>
+
+namespace vishnevskiy
+{
+  struct vertex
+  {
+    std::string out;
+    std::string in;
+  }
+
+  struct graph
+  {
+    std::string name;
+    vishnevskiy::HashTable<vertex, int, std::size_t(*)(const vertex&), bool(*)(const vertex&, const vertex&)> vertexes;
+  }
+
+  size_t vertexHasher(const vertex& v)
+  {
+    boost::hash2::siphash_64 hash;
+    hash.update(v.out.data(), v.out.size());
+    hash.update(v.in.data(), v.in.size());
+    return hash.result();
+  }
+
+  bool vertexEq(const vertex& first, const vertex& second)
+  {
+    return first.out == second.out && first.in == second.in;
+  }
+
+  size_t graphHasher(const graph& g)
+  {
+    boost::hash2::siphash_64 hash;
+    hash.update(g.name.data(), g.name.size());
+    return hash.result();
+  }
+
+  bool graphEq(const graph& graph1, const graph& graph2)
+  {
+    if ((graph1.name != graph2.name) || (graph1.vertexes.getSize() != graph2.vertexes.getSize()))
+    {
+      return false;
+    }
+  }
+}
+
+#endif
