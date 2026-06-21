a748a2e2860a07f44d7926d6751e18331c85158d
Oleg <n7o.nto@yandex.ru>
2026-06-21 23:18:14 +0300

    Fixed tree hpp

--- vishnevskiy.oleg/S4/Tree.hpp
+++ vishnevskiy.oleg/S4/Tree.hpp
@@ -1,29 +1,70 @@
 #ifndef HASHTABLE_HPP
 #define HASHTABLE_HPP
 
+#include <cstddef>
+#include <stdexcept>
+
 namespace vishnevskiy
 {
+  template <class Key, class Value, class Compare>
+  class BSTree;
+
   template <class Key, class Value>
   class BSIterator
   {
-    BSTree<Key, Value, Compare>* curr;
+    private:
+      Node<Key, Value>* curr;
+    public:
+      void next();
+      void prev();
+      Value getVal();
+      Node<Key, Value>* getNode() const;
   };
 
   template <class Key, class Value>
   class BSConstIterator
   {
-    BSTree<Key, Value, Compare>* curr;
+    private:
+      BSTree<Key, Value, Compare>* curr;
+    public:
+      void next();
+      void prev();
+      const Value getVal() const;
+      const Node<Key, Value>* getNode() const;
+  };
+
+  template <class Key, class Value>
+  struct Node
+  {
+    private:
+      Key* key;
+      Value* val;
+      Node* parent;
+      Node* left;
+      Node* right;
+      size_t height;
+    public:
+      Node();
+      Node(const Key& k, const Value& v, Node* p);
+      ~Node();
+      friend class BSTree<Key, Value, Compare>;
   };
 
   template <class Key, class Value, class Compare>
   class BSTree
   {
     private:
-      BSTree<Key, Value, Compare>* parent;
-      BSTree<Key, Value, Compare>* left;
-      BSTree<Key, Value, Compare>* right;
-      Value* val;
+      Node<Key, Value>* fakeRoot;
+      Node<Key, Value>* fakeLeaf;
+      Compare cmp;
+      size_t nodes;
+      Node<Key, Value>* findByKey(const Key& k) const;
     public:
+      BSTree();
+      ~BSTree();
+      BSTree(const BSTree& other);
+      BSTree& operator=(const BSTree& other);
+      bool has(const Key& k) const;
       void push(Key k, Value v);
       Value get(Key k);
       Value drop(Key k);
@@ -37,3 +78,5 @@ namespace vishnevskiy
       size_t height();
   };
 }
+
+#endif
