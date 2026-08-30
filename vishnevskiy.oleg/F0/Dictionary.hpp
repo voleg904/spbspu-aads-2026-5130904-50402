01ca243bcaf8668afd5c9fba61b3291cb1656366
Oleg <n7o.nto@yandex.ru>
2026-08-31 01:25:45 +0300

    Added constructor, destructor and commands

--- vishnevskiy.oleg/F0/Dictionary.hpp
+++ vishnevskiy.oleg/F0/Dictionary.hpp
@@ -22,7 +22,21 @@ namespace vishnevskiy
       stringHash_t hashFunc;
       stringEq_t eqFunc;
 
+  public:
+    DictionaryManager(size_t capacity, stringHash_t hash, stringEq_t eq);
+    ~DictionaryManager();
 
+    void load(const std::string& filename, const std::string& dictName);
+    void deleteDict(const std::string& dictName);
+    void merge(const std::string& dict1, const std::string& dict2, const std::string& result);
+    void find(const std::string& dictName, const std::string& pos, const std::string& word);
+    void save(const std::string& filename, const std::string& dictName);
+    void addWord(const std::string& dictName, const std::string& word, const std::string& pos, const std::string& translations);
+    void addTranslation(const std::string& dictName, const std::string& word, const std::string& translation);
+    void deleteWord(const std::string& dictName, const std::string& word);
+    void deleteTranslation(const std::string& dictName, const std::string& word, const std::string& translation);
+    void changePos(const std::string& dictName, const std::string& word, const std::string& newPos);
+    void findTranslation(const std::string& dictName, const std::string& translation);
   };
 }
 
